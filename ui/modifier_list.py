6f0c4d37cc4dd6d679f4ef0edb950ad34996bd7a
Salai Vedha Viradhan <salaivv@gmail.com>
2025-05-16 22:02:13 +0530

    Use builtin method to get modifier icon

--- ui/modifier_list.py
+++ ui/modifier_list.py
@@ -12,14 +12,10 @@ class MODIFIER_UL_modifier_stack(UIList):
         if self.layout_type in {'DEFAULT', 'COMPACT'}:
             row = layout.row()
             
-            row.prop(md, 'name', text="", emboss=False, icon_value=1, icon = self.get_mod_icon(md))
+            row.prop(md, 'name', text="", emboss=False, icon_value=layout.icon(md))
             row.prop(md, 'show_render', text="", emboss=False, icon_only=True)
             row.prop(md, 'show_viewport', text="", emboss=False, icon_only=True)
 
         elif self.layout_type == 'GRID':
             layout.alignment = 'CENTER'
             layout.label(text='', icon_value=icon)
-            
-    def get_mod_icon(self, md):
-        md_icon = bpy.types.Modifier.bl_rna.properties['type'].enum_items[md.type].icon
-        return md_icon
