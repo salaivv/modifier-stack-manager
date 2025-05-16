8bb605c37008521f74b6cfb560d6eb530a7c1cc8
Salai Vedha Viradhan <salaivv@gmail.com>
2025-05-16 22:38:40 +0530

    Removed unnecessary row creation

--- ui/modifier_list.py
+++ ui/modifier_list.py
@@ -10,11 +10,9 @@ class MODIFIER_UL_modifier_stack(UIList):
         md = item
         
         if self.layout_type in {'DEFAULT', 'COMPACT'}:
-            row = layout.row()
-            
-            row.prop(md, 'name', text="", emboss=False, icon_value=layout.icon(md))
-            row.prop(md, 'show_render', text="", emboss=False, icon_only=True)
-            row.prop(md, 'show_viewport', text="", emboss=False, icon_only=True)
+            layout.prop(md, 'name', text="", emboss=False, icon_value=layout.icon(md))
+            layout.prop(md, 'show_viewport', text="", emboss=False, icon_only=True)
+            layout.prop(md, 'show_render', text="", emboss=False, icon_only=True)
 
         elif self.layout_type == 'GRID':
             layout.alignment = 'CENTER'
