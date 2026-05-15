1f93c5479400081e862b2161c360577357d369df
Salai Vedha Viradhan <salaivv@gmail.com>
2026-05-15 16:32:47 +0530

    Refactor

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,15 +1,17 @@
+import bpy
 from .modifier_list import MODIFIER_UL_modifier_stack
 
 
+classes = [
+	MODIFIER_UL_modifier_stack,
+]
+
+
 def register():
-	import bpy
-	bpy.utils.register_class(MODIFIER_UL_modifier_stack)
+	for cl in classes:
+		bpy.utils.register_class(cl)
 
 
 def unregister():
-	import bpy
-	bpy.utils.unregister_class(MODIFIER_UL_modifier_stack)
-
-
-if __name__ == "__main__":
-    register()
+	for cl in reversed(classes):
+		bpy.utils.unregister_class(cl)
