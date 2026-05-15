c1fe9b5cdcbe847cc5bef36af3fcb4edf77a056e
Salai Vedha Viradhan <salaivv@gmail.com>
2026-05-15 11:23:47 +0530

    Revert addon reload code

--- src/__init__.py
+++ src/__init__.py
@@ -17,28 +17,10 @@
 # ##### END GPL LICENSE BLOCK #####
 
 
-if "bpy" in locals():
-    import importlib
-
-    mods = (
-        preferences,
-        operators,
-        ui
-    )
-
-    for mod in mods:
-        importlib.reload(mod)
-
-    print("Add-on reloaded: Modifier Stack Manager")
-
-else:
-    import bpy
-
-    from . import (
-            preferences,
-            operators,
-            ui
-        )
+import bpy
+from . import preferences
+from . import operators
+from . import ui
 
 
 def draw(self, context):
