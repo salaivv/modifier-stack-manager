1083fc9d6c2d019baa28d8e315d93b40c223e758
Salai Vedha Viradhan <salaivv@gmail.com>
2026-06-04 19:09:44 +0530

    Cleanup

--- src/ui/modifier_list.py
+++ src/ui/modifier_list.py
@@ -12,19 +12,19 @@ class MODIFIER_UL_modifier_stack(UIList):
         self, context, layout, data, item, 
         icon, active_data, active_propname, index
     ):
-        md = item
+        modifier = item
         
         if self.layout_type in {'DEFAULT', 'COMPACT'}:
-            layout.prop(md, 'name', text="", emboss=False, icon_value=layout.icon(md))
+            layout.prop(modifier, "name", text="", emboss=False, icon_value=layout.icon(modifier))
 
             if addon_prefs.preferences.use_show_viewport_toggle:
-                layout.prop(md, 'show_viewport', text="", emboss=False, icon_only=True)
+                layout.prop(modifier, "show_viewport", text="", emboss=False, icon_only=True)
 
             if addon_prefs.preferences.use_show_render_toggle:
-                layout.prop(md, 'show_render', text="", emboss=False, icon_only=True)
+                layout.prop(modifier, "show_render", text="", emboss=False, icon_only=True)
 
             if addon_prefs.preferences.use_show_in_editmode_toggle:
-                layout.prop(md, 'show_in_editmode', text="", emboss=False, icon_only=True)
+                layout.prop(modifier, "show_in_editmode", text="", emboss=False, icon_only=True)
 
         elif self.layout_type == 'GRID':
             layout.alignment = 'CENTER'
